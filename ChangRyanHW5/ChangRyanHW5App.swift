ceefac40f10458bec1e2943ee3b241e0120b2098
Ryan Chang <Ryanchang0529@gmail.com>
2025-03-11 22:42:00 -0700

    Initial Commit

---
+++ ChangRyanHW5/ChangRyanHW5App.swift
@@ -0,0 +1,17 @@
+//
+//  ChangRyanHW5App.swift
+//  ChangRyanHW5
+//
+//  Created by Ryan Chang on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ChangRyanHW5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChangRyanHW5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChangRyanHW5
+//
+//  Created by Ryan Chang on 3/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
